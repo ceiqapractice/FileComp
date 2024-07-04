984c606b8632cde78c20fe3c52efc1147909b6f2
ceiqapractice <110914539+ceiqapractice@users.noreply.github.com>
2024-07-04 06:55:30 +0000

    changes

---
+++ playwright.config.js
@@ -0,0 +1,10 @@
+// playwright.config.js
+
+module.exports = {
+    testDir: './tests', // Directory where your tests are located
+    timeout: 30000,
+    use: {
+      headless: false,
+    },
+  };
+  
